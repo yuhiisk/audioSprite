d3ffc9977db11c238633199b1580f9def73b8cbc
yuhiisk <youheiisokawa@gmail.com>
2013-08-20 01:43:22 +0900

    first commit

---
+++ js/audioSprite.js
@@ -0,0 +1,22 @@
+/**
+ * audioSprite.js
+ * @author Youheiisokawa
+ * @license MIT Lisence
+ */
+
+;(function(w, d, undefined) {
+	'use strict';
+
+	var AudioSprite = function() {
+
+	};
+
+	AudioSprite.prototype.play = function(i) {
+		
+	};
+
+	AudioSprite.prototype.pause = function(i) {
+		
+	};
+
+})(this, this.document);
